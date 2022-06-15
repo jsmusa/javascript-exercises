034663b22b18faa12c3c6b00625264bf40372f91
Jojo <jonathansardeamusa@gmail.com>
2022-06-15 23:45:41 +0800

    finished exercise 4: removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...entries) {
+    let newArray = array;
+    for (const entry of entries) {
+        function check(removedValue) {
+            return removedValue !== entry; 
+        }
+        newArray = newArray.filter(check);
+    }
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
