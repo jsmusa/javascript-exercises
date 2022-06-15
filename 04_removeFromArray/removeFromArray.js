4c755a14ac4a665731c841d06d510dd7675b98cd
Jojo <jonathansardeamusa@gmail.com>
2022-06-15 23:58:09 +0800

    added alternate solution on exercise 4: removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,15 @@
 const removeFromArray = function(array, ...entries) {
     let newArray = array;
     for (const entry of entries) {
-        function check(removedValue) {
+        /*function check(removedValue) {
             return removedValue !== entry; 
         }
-        newArray = newArray.filter(check);
+        newArray = newArray.filter(check);*/
+        for (let i=0; i<newArray.length; i++) {
+            if (newArray[i] === entry) {
+                newArray.splice(i,1);
+            }
+        }
     }
     return newArray;
 };
