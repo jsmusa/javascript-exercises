11af83beaa7560a38bdb6d18b6b4c93df9a06ad2
Jojo <jonathansardeamusa@gmail.com>
2022-06-15 22:35:49 +0800

    finished exercise 2: repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string,num) {
+    let multiString = "";
+    if (num >= 0) {
+    for  (i=0; i<num; i++) {
+        multiString += string;
+    }
+    } else {
+        multiString = "ERROR";
+    }
+    return multiString;
+}
 // Do not edit below this line
 module.exports = repeatString;
