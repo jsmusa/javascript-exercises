687b6137792c3540b197579a45bf316d0d4626f3
Jojo <jonathansardeamusa@gmail.com>
2022-06-16 14:30:19 +0800

    finished exercise 5: sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num,numTwo) {
+    //returns ERROR if num or numTwo is negative or not a number
+    if (num <0 || numTwo<0 || typeof num !== "number" || typeof numTwo !== "number") {
+        return "ERROR";
+    }
+    let sum = 0;
+    //sums all numbers from num upto numTwo
+    while (num !== numTwo) {
+        sum += num;
+        if (num>numTwo) num--;
+        else num++;
+    }
+    //needs to add numTwo since loop breaks when num === numTwo
+    return sum+numTwo;
 };
 
 // Do not edit below this line
