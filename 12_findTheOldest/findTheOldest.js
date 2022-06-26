7d47ae1e18bae8bb6b7cc7530efe49f274d91f82
Jojo <jonathansardeamusa@gmail.com>
2022-06-26 23:23:00 +0800

    finished exercise 12: findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    for (const element of array) {
+        if (element.yearOfDeath === undefined) {
+            element.yearOfDeath = new Date().getFullYear();
+        } else continue;
+    }
+    const sorted = array.sort((first,second) => {
+        return ((first.yearOfDeath - first.yearOfBirth) > (second.yearOfDeath - second.yearOfBirth) ? -1 : 1);
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
