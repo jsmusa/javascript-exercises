6a47de33d5aad6674686b15f602bde073d2da3ad
Jojo <jonathansardeamusa@gmail.com>
2022-06-26 18:39:49 +0800

    finished exercise 9: palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // let newString = string.replace(/,|\.|\!| /g,"");
+    let newString = string.replace(/[,.! ]/g,"");
+    let reverse = "";
+    for (let i = newString.length; i > 0; i--) {
+        reverse += newString.slice(i-1,i); 
+    }
+    // return(newString);
+    return newString.toLowerCase() === reverse.toLowerCase(); 
 };
 
 // Do not edit below this line
