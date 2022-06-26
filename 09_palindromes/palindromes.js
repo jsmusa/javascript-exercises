216eb5153d131eef9468f7d2d8ff3138b6774960
Jojo <jonathansardeamusa@gmail.com>
2022-06-26 18:42:04 +0800

    added solution from odin

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -9,5 +9,15 @@ const palindromes = function (string) {
     return newString.toLowerCase() === reverse.toLowerCase(); 
 };
 
+// const palindromes = function(string) {
+//     processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+//     return (
+//       processedString
+//         .split("")
+//         .reverse()
+//         .join("") == processedString
+//     );
+//  };
+
 // Do not edit below this line
 module.exports = palindromes;
