023a9f0a8e698b33e0d31bd75373235a9a8ca33d
Jojo <jonathansardeamusa@gmail.com>
2022-06-26 19:04:43 +0800

    finished exercise 10:fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    // initial values
+    let first = 0;
+    let second = 1;
 
+    for (let i=0; i<number; i++) {
+        let sum = first + second;
+        first = second;
+        second = sum;
+    }
+    return first;
 };
 
 // Do not edit below this line
